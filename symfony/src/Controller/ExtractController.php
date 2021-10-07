54f19d8a89f99414aa18181e1832efa0e02fac48
dragomirt <dragomirt22@gmail.com>
2021-10-07 11:19:58 +0300

    Moves files on upload

--- symfony/src/Controller/ExtractController.php
+++ symfony/src/Controller/ExtractController.php
@@ -6,18 +6,26 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ExtractController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
     /** @Route(name="extract", path="/extract", methods={"POST"})  */
-    public function __invoke()
+    public function __invoke(Request $request, LoggerInterface $logger)
     {
-//        $em = $this->getDoctrine()->getManager();
-//        /** @var ProductRepository $repo */
-//        $repo = $em->getRepository(Product::class);
-//        $repo->findAll();
+        $logger->info("Got a request for extraction");
+        $raw_file = $request->files->get("file_to_extract");
+
+        // Saving file
+        $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
+        $raw_file->move($destination, $raw_file->getClientOriginalName());
+        $logger->info("Saved {$raw_file->getClientOriginalName()} to public storage");
+
+
         return new JsonResponse(['extracted'=> true]);
     }
 }
