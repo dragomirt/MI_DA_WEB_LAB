cb02d629d91116fb2f9be249b10ddd1d1ea87a92
dragomirt <dragomirt22@gmail.com>
2021-11-14 23:16:18 +0200

    Updates the scanning job

--- api/app/app/Jobs/ProcessPdf.php
+++ api/app/app/Jobs/ProcessPdf.php
@@ -2,12 +2,15 @@
 
 namespace App\Jobs;
 
+use App\Models\FileContent;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Spatie\PdfToText\Pdf;
 
 class ProcessPdf implements ShouldQueue
 {
@@ -38,6 +41,28 @@ class ProcessPdf implements ShouldQueue
             $this->fail(new \Exception("No file available."));
         }
 
+        if (null === $this->media_id) {
+            $this->fail(new \Exception("No media found."));
+        }
 
+        $content = null;
+        try {
+            $content = (new Pdf())
+                ->setPdf($this->filepath)
+                ->setOptions(['layout', 'r 96'])
+                ->addOptions(['f 1'])
+                ->setScanOptions(['-l ron+rus+eng', '--skip-text'])
+                ->scan()
+                ->decrypt()
+                ->text();
+        } catch (\Exception $exception) {
+            Log::error($exception->getMessage());
+            Log::error($exception->getTraceAsString());
+        }
+
+        $file_content = new FileContent();
+        $file_content->media_id = $this->media_id;
+        $file_content->content = $content;
+        $file_content->save();
     }
 }
--- api/app/app/Models/FileContent.php
+++ api/app/app/Models/FileContent.php
@@ -5,4 +5,7 @@ namespace App\Models;
 class FileContent extends \Illuminate\Database\Eloquent\Model
 {
 
+    protected $table = 'file_contents';
+
+    protected $fillable = ['media_id', 'content'];
 }
