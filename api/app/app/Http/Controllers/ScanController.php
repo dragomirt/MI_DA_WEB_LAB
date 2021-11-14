15929bcfbc2ee1f3ba881418e29aa7b1d3e8dfcd
dragomirt <dragomirt22@gmail.com>
2021-11-15 01:24:00 +0200

    Makes file processing in sync

--- api/app/app/Http/Controllers/ScanController.php
+++ api/app/app/Http/Controllers/ScanController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ScanRequest;
 use App\Jobs\ProcessPdf;
+use App\Models\FileContent;
 use Illuminate\Http\Client\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Spatie\PdfToText\Pdf;
 
 class ScanController
 {
@@ -20,8 +23,38 @@ class ScanController
         $media = $user->addMediaFromRequest('file')
             ->toMediaCollection('files');
 
-        ProcessPdf::dispatch($media->id, $media->getPath());
+        $media_id = $media->id;
 
-        return response()->json([], JsonResponse::HTTP_NO_CONTENT);
+        $filepath = $media->getPath();
+
+        if (null === $filepath) {
+            $this->fail(new \Exception("No file available."));
+        }
+
+        if (null === $media_id) {
+            $this->fail(new \Exception("No media found."));
+        }
+
+        $content = null;
+        try {
+            $content = (new Pdf())
+                ->setPdf($filepath)
+                ->setOptions(['layout', 'r 96'])
+                ->addOptions(['f 1'])
+                ->setScanOptions(['-l ron+rus+eng', '--skip-text'])
+                ->scan()
+                ->decrypt()
+                ->text();
+        } catch (\Exception $exception) {
+            Log::error($exception->getMessage());
+            Log::error($exception->getTraceAsString());
+        }
+
+        $file_content = new FileContent();
+        $file_content->media_id = $media_id;
+        $file_content->content = $content;
+        $file_content->save();
+
+        return response()->json(['content' => $content], JsonResponse::HTTP_OK);
     }
 }
